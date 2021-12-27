1bfc9f92aadd32309469453cdd9cbaf15beb967a
SDETEMRE <dincsoyemre@gmail.com>
2021-12-27 20:33:07 +0300

    second commit

--- src/test/java/com/cydeo/tests/Test_Emre.java
+++ src/test/java/com/cydeo/tests/Test_Emre.java
@@ -3,5 +3,6 @@ package com.cydeo.tests;
 public class Test_Emre {
     public static void main(String[] args) {
         System.out.println("First change from Emre");
+        System.out.println("Second change from Emre");
     }
 }
