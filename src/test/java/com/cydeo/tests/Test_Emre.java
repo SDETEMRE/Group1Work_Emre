93249a21cb178ddc6c4c8a87fdc19fafab512f50
SDETEMRE <dincsoyemre@gmail.com>
2021-12-27 20:40:53 +0300

    first commit from Emre branch

--- src/test/java/com/cydeo/tests/Test_Emre.java
+++ src/test/java/com/cydeo/tests/Test_Emre.java
@@ -4,5 +4,6 @@ public class Test_Emre {
     public static void main(String[] args) {
         System.out.println("First change from Emre");
         System.out.println("Second change from Emre");
+        System.out.println("First change of Emre branch");
     }
 }
